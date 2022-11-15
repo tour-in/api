3c67747c78eaed2f3b66c08b5cfb6ef906cf47eb
Edivaldo Luz <edivaldo.luz@s3digital.com.br>
2022-11-15 08:11:26 -0300

    feat: express and base

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routes = (app) => {
+    app.route("/").get((req, res) => {
+      res.status(200).send({
+        resultado: "rob.valdo 1.0.0 - ALPHA",
+      });
+    });
+  
+    app.use(
+      express.json(),
+    );
+  };
+  
+  module.exports = routes;
