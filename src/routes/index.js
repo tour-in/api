50c20f920255455eb048a4f3839e1e19ef635f0d
Edivaldo Luz <edivaldo.luz@s3digital.com.br>
2022-11-15 08:21:45 -0300

    feat: sequelize e organização de pastas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 
 const routes = (app) => {
-    app.route("/").get((req, res) => {
-      res.status(200).send({
-        resultado: "rob.valdo 1.0.0 - ALPHA",
-      });
+  app.route("/").get((req, res) => {
+    res.status(200).send({
+      resultado: "rob.valdo 1.0.0 - ALPHA",
     });
-  
-    app.use(
-      express.json(),
-    );
-  };
-  
-  module.exports = routes;
+  });
+
+  app.use(express.json());
+};
+
+module.exports = routes;
